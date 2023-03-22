7314c932383267c3fb4a68849a46928097f7b1bd
Ahmed Belal <ahmedbelal.asurt@gmail.com>
2023-03-23 01:37:35 +0200

    feat: update roar_supervisor

--- supervisor/src/roar_supervisor.py
+++ supervisor/src/roar_supervisor.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python3
 
+
 import rospy
 import roslaunch
 from roar_msgs.msg import Mode
-
-
-class Launcher():
-
-    def __init__(self):
-        self.manual_path = "~/roar_ws/supervisor/launch/manual_mode.launch"
-        self.auto_path = "~/roar_ws/supervisor/launch/auto_mode.launch"
-        self.launch = roslaunch.scriptapi.ROSLaunch()
-        self.launch.start()
 
 
 class Handler():
@@ -19,49 +11,73 @@ class Handler():
     def __init__(self):
         # Initialize node and sleep rate
         rospy.init_node("roar_supervisor")
+        rospy.loginfo("roar_supervisor initialized")
         self.rate = rospy.Rate(10)
         # Subscribe
         rospy.Subscriber("/base/command/mode", 
                          Mode, 
                          self.command_callback)
-        # Initialize required variables
-        self.mode_switch = False
-        self.current_mode = "Manual"
+        # Initialize the roslaunch objects
+        self.init_launchers()
         # Initialize manual mode nodes
-        self.mode_init()
+        self.init_mode()
         # Loop and wait for mode switch commands
         self.loop()
     
     # Gets called everytime a mode command is received
     def command_callback(self, rec_msg):
-        self.mode = rec_msg
+        self.mode_command = rec_msg
         self.mode_switch = True
 
+    def init_launchers(self):
+        # Launch files paths
+        self.manual_path = "~/roar_ws/supervisor/launch/manual_test.launch"
+        self.autonomous_path = "~/roar_ws/supervisor/launch/autonomous_test.launch"
+        # Generate uuids
+        self.manual_uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+        self.autonomous_uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+        # Create ROSLaunchParent objects
+        self.manual_launcher = roslaunch.parent.ROSLaunchParent(self.manual_uuid
+                                                                , [self.manual_path])
+        self.autonomous_launcher = roslaunch.parent.ROSLaunchParent(self.autonomous_uuid
+                                                                , [self.autonomous_path])
+        rospy.loginfo("ROSLaunchParent objects initialized")
+
     # Gets called only one time to initialize ROAR in Manual mode
-    def mode_init(self):
+    def init_mode(self):
+        # Initialize required variables
+        self.mode_switch = False
+        self.current_mode = "Manual Mode"
         rospy.loginfo("Launching ROAR in Manual mode")
         # Launch manual mode nodes
+        self.manual_launcher.start()
 
     # Gets called everytime 
     def switch_mode(self):
         if self.mode_switch == True:
-            if self.mode.mode == self.mode.AUTO:
-                if self.current_mode == "Manual":
-                    rospy.loginfo("Shutting down Autonomous mode nodes")
-                    # Shutdown auto mode nodes
-                    rospy.loginfo("Launching Manual mode nodes")
-                    # Launch manual mode nodes
-                else:
+            if self.mode_command.mode == self.mode_command.AUTO:
+                if self.current_mode == "Manual Mode":
+                    rospy.loginfo("Shutting down Manual Mode nodes")
+                    self.manual_launcher.shutdown()
+                    rospy.loginfo("Launching Autonomous Mode nodes")
+                    self.autonomous_launcher.start()
+                    self.current_mode == "Autonomous Mode"
+                elif self.current_mode == "Autonomous Mode":
                     rospy.logwarn("Already in Autonomous Mode")
             elif self.mode.mode == self.mode.MANUAL:
-
-
-                
+                if self.current_mode == "Manual Mode":
+                    rospy.logwarn("Already in Manual Mode")
+                elif self.current_mode == "Autonomous Mode":
+                    rospy.loginfo("Shutting down Autonomous Mode nodes")
+                    self.autonomous_launcher.shutdown()
+                    rospy.loginfo("Launching Manual Mode nodes")
+                    self.manual_launcher.start()
+                    self.current_mode == "Manual Mode"
             self.mode_switch = False
 
+    # Loop
     def loop(self):
         while not rospy.is_shutdown():
-            
             self.rate.sleep()
         
 
