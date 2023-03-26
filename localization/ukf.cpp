57d09cbcf0e96939eb844188b37ffd51868eb84f
Omar Tarek Omar <tarekomar37@gmail.com>
2023-03-26 13:46:30 +0200

    Update ukf.cpp
    wrote bare-bone code

--- localization/ukf.cpp
+++ localization/ukf.cpp
@@ -1,7 +1,8 @@
 #include <ros/ros.h>
 #include <std_msgs/Float32MultiArray.h>
-#include <eigen3/Eigen/Core>
 #include <eigen3/Eigen/Dense>
+#include <eigen3/Eigen/Cholesky>
+#include <cmath>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
@@ -9,20 +10,33 @@ using Eigen::VectorXd;
 float x = 0;
 float y = 0;
 float z = 0;
+std::vector<float> Vel_arr = {0, 0, 0, 0, 0, 0};
+std::vector<float> IMU_arr;
+std::vector<float> IMU_arr_old;
 
-void Vel_Callback(const std_msgs::Float32MultiArray::ConstPtr& msg)
+void Vel_Callback(const std_msgs::Float32MultiArray::ConstPtr& msg)                            // callback of encoder readings
 {
   ROS_INFO("I heard Velocities");
-  x = 65;
+  Vel_arr = msg->data;
 }
 
-void IMU_Callback(const std_msgs::Float32MultiArray::ConstPtr& msg)
+void IMU_Callback(const std_msgs::Float32MultiArray::ConstPtr& msg)                            // callback of IMU readings
 {
   ROS_INFO("I heard IMU");
-  y = 50;
+  IMU_arr = msg->data;
 }
 
-int main(int argc, char *argv[])
+void Predict()                                                                                 // prediction function (where system model goes)
+{
+
+}
+
+void Estimate()                                                                                // estimation function (where we update prediction readings using IMU)
+{
+
+}
+
+int main(int argc, char *argv[])                                                               // initialization of ros node and other variables
 {
   ros::init(argc, argv, "localization");
   ros::NodeHandle nh;
@@ -31,13 +45,31 @@ int main(int argc, char *argv[])
   ros::Subscriber sub2 = nh.subscribe("IMU", 10, IMU_Callback);
   std_msgs::Float32MultiArray coordinates;
   ros::Rate rate(10);
-  while (ros::ok())
+  std::vector<float> coordinates_arr = {x, y, z};
+  Eigen::Matrix3f model_noise;
+  Eigen::Matrix3f measure_noise;
+  model_noise << 1.0, 2.0, 3.0,
+                 4.0, 5.0, 6.0,
+                 7.0, 8.0, 9.0;
+
+  measure_noise << 1.0, 2.0, 3.0,
+                   4.0, 5.0, 6.0,
+                   7.0, 8.0, 9.0;               
+
+  Eigen::Vector3f pose(1.0, 2.0, 3.0);       
+
+  while (ros::ok())                                                                            // while (1) loop
   {
-    std::vector<float> coordinates_arr = {x, y, z};
     coordinates.data = coordinates_arr;
-    pub.publish(coordinates);
-    ros::spinOnce();
-    rate.sleep();
+    if (IMU_arr != IMU_arr_old)
+    {
+      Predict();
+      Estimate();
+      pub.publish(coordinates);
+      ros::spinOnce();
+      rate.sleep();
+      IMU_arr_old = IMU_arr;
+    }
   }
   return 0;
 }
