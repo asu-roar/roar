2746a27f2773aa99a0925174cc23724cb2f4349e
Ahmed Belal <ahmedbelal.asurt@gmail.com>
2023-03-26 23:44:13 +0200

    feat: update mode_sender

--- supervisor/test/mode_sender.py
+++ supervisor/test/mode_sender.py
@@ -1,11 +1,12 @@
 import rospy
 import time
-from roar_msgs.msg import Mode
+from std_msgs.msg import UInt8
 
 if __name__=="__main__":
     rospy.init_node("mode_sender_node")
-    pub = rospy.Publisher("/base/command/mode", Mode, queue_size=2)
-    time.sleep(2)
-    msg = Mode()
-    msg.mode = msg.MANUAL
+    pub = rospy.Publisher("/base/command/mode", UInt8, queue_size=1)
+    time.sleep(1)
+
+    msg = UInt8()
+    msg.data = 0
     pub.publish(msg)
