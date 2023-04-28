a51623430f4041572549e387fb7edd2098722e7e
Ahmed Belal <ahmedbelal.asurt@gmail.com>
2023-04-28 20:17:34 +0300

    feat: add handler exception class

--- supervisor/src/supervisor/supervisor_exceptions.py
+++ supervisor/src/supervisor/supervisor_exceptions.py
@@ -30,14 +30,13 @@ class ModuleError(SupervisorError):
 class HandlerError(SupervisorError):
 
     """
-    UNUSED
     Subclass of SupervisorError. Base class for all module handler exceptions.
     """
 
     pass
 
 
-# --------------------------------- Sub Classes ---------------------------------
+# --------------------------------- Module Sub Classes ---------------------------------
 
 
 class ModuleLaunchError(ModuleError):
@@ -65,6 +64,17 @@ class ModuleStatusError(ModuleError):
     """
     Subclass of ModuleError.
     Errors related to the shutting down of a module.
+    """
+
+    pass
+
+# --------------------------------- Handler Sub Classes ---------------------------------
+
+class HandlerInitError(HandlerError):
+
+    """
+    Subclass of HandlerError.
+    Errors related to the initializing of a ModuleHandler.
     """
 
     pass
