d7ce7421f670869eb21ba26f44f983c0b3ea3094
Ahmed Belal <ahmedbelal.asurt@gmail.com>
2023-05-02 21:27:12 +0300

    feat: update supervisor methods

--- supervisor/scripts/roar_supervisor.py
+++ supervisor/scripts/roar_supervisor.py
@@ -2,136 +2,75 @@
 
 
 import rospy
-import roslaunch.rlutil
-import roslaunch.parent
-from roar_msgs.msg import ModeCommand
-from supervisor.module import Module
-
-
-class Launcher():
-
-    def __init__(self, mode) -> None:
-        # Configure the launcher with the required mode
-        self.mode = mode
-        self.mode_config()
-        # Create ROSLaunchParent object
-        rospy.loginfo("Creating {} launcher object" .format(self.mode))
-        self.uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-        self.launcher = roslaunch.parent.ROSLaunchParent(self.uuid, 
-                                                         [self.path])
-        # roslaunch.configure_logging(self.uuid)
-
-    # Configure launcher path and delay based on selected mode
-    def mode_config(self) -> None:
-        # Manual Mode configuration
-        if self.mode.mode == self.mode.MANUAL:
-            # Manual Mode launch file path
-            self.path = "/home/belal/roar_ws/src/supervisor/launch/manual_test.launch"
-            # Manual Mode delay
-            self.delay = 0
-            # Used for logging purposes
-            self.mode = "Manual Mode"
-        # Autonomous Mode configuration
-        elif self.mode.mode == self.mode.AUTONOMOUS:
-            # Autonomous Mode launch file path
-            self.path = "/home/belal/roar_ws/src/supervisor/launch/autonomous_test.launch"
-            # Autonomous Mode delay
-            self.delay = 5
-            # Used for logging purposes
-            self.mode = "Autonomous Mode"
-
-    # A method to launch the created launcher
-    def launch(self) -> None:
-        # Launch the nodes after the required delay
-        rospy.loginfo("Launching {}" .format(self.mode))
-        rospy.sleep(self.delay)
-        self.launcher.start()
-        # Check if the nodes were successfully launched
-        while not self.launcher.pm.is_alive():
-            rospy.loginfo("Waiting for Alive")
-            rospy.sleep(0.1)
-        rospy.loginfo("{} was launched successfully" .format(self.mode))
-
-    # A method to shutdown and delete the created launcher
-    def shutdown(self) -> None:
-        # Shutdown the nodes
-        rospy.loginfo("Shutting down {}" .format(self.mode))
-        self.launcher.shutdown()
-        # Check if the nodes were successfully shutdown
-        while self.launcher.pm.is_alive():
-                rospy.sleep(0.1)
-        rospy.loginfo("{} was shutdown successfully" .format(self.mode))
-        # Delete the current Launcher object to free memory space
-        del self
+from typing import List
+from roar_msgs.msg import ModeCommand, ModuleStatus
+from supervisor.module_handler import ModuleHandler
 
 
 class NodeHandler():
 
     def __init__(self) -> None:
-        # Initialize the ROS node
+        # Initialize the node
         self.init_node()
-        # Initialize ROAR in Manual Mode
+        # Initialize rover in manual mode
         self.init_roar()
-        # Loop and wait for mode commands
+        # Loop in the desired mode
         self.loop()
 
     def init_node(self) -> None:
-        # Initialize node and sleep rate
         rospy.init_node("roar_supervisor")
         rospy.loginfo("roar_supervisor node initialized")
-        # Check mode commands at 2 Hz
-        self.rate = rospy.Rate(2)
-        # Subscribe
-        rospy.Subscriber("/base/command/mode", 
-                         ModeCommand, 
-                         self.command_callback
-                         )
-        
-    # Gets called only one time to launch ROAR in Manual Mode
+        self.rate = rospy.Rate(1)
+        rospy.Subscriber(
+            "/base/command/mode", ModeCommand, self.command_callback)
+     
     def init_roar(self) -> None:
-        # Initialize mode variables
-        # Boolean to determine whether a Mode command was received or not
-        self.received = False
-        # Initialize mode variable
+        # Configuration variables
+        self.json_path: str = "/home/belal/roar_ws/src/supervisor/config/modules.json"
+        self.manual_key: str = "manual_modules"
+        self.autonomous_key: str = "autonomous_modules"
+        # Initialize the module handler
+        self.module_handler = ModuleHandler()
+        self.module_handler.parse_json(self.json_path)
+        # Initialize manual mode
         self.mode = ModeCommand()
         self.mode.mode = self.mode.MANUAL
-        # Create and launch a Launcher object in Manual Mode
-        rospy.loginfo("Waking up ROAR in Manual Mode")
-        self.launcher = Launcher(self.mode)
-        self.launcher.launch()
+        self.received: bool = False
+        # Launch manual mode modules
+        self.module_handler.clear_and_load(self.manual_key)
+        self.module_handler.launch_all()
+
+    # Gets called everytime a mode command is received
+    def command_callback(self, rec_msg: ModeCommand) -> None:
+        self.rec_mode = rec_msg
+        self.received = True
 
     # Loop until a mode command is received
     def loop(self) -> None:
         while not rospy.is_shutdown():
-            # Check if a command is received
             if self.received == True:
                 # Check if the received mode is different from current mode
-                if self.rec_mode != self.mode:
+                if self.rec_mode.mode != self.mode.mode:
                     # Switch to the received mode
+                    self.mode = self.rec_mode
                     self.switch_mode()
-                # Print warnings in case received mode is the same as the current mode
                 else:
-                    rospy.logwarn("ROAR is already in the desired mode!")
-                # Reset the received mode flag
+                    rospy.logwarn(
+                        "Rover is already in the desired mode!")
                 self.received = False
-            # Sleep at the required rate
+            else:
+                self.module_handler.keep_online()
+                status_list: List[ModuleStatus] = self.module_handler.get_states()
+                rospy.loginfo(status_list)
             self.rate.sleep()
 
-    # Gets called everytime a mode command is received
-    def command_callback(self, rec_msg) -> None:
-        self.rec_mode = rec_msg
-        self.received = True
-
-    # Gets called everytime a mode switch is required
     def switch_mode(self) -> None:
-        # Shutdown current active mode
-        self.launcher.shutdown()
-        # Change mode variable to correct value
-        self.mode = self.rec_mode
-        # Create and launch a Launcher object in the new mode
-        self.launcher = Launcher(self.mode)
-        self.launcher.launch()
-
+        self.module_handler.shutdown_all()
+        if self.mode.mode == self.mode.MANUAL:
+            self.module_handler.clear_and_load(self.manual_key)
+        elif self.mode.mode == self.mode.AUTONOMOUS:
+            self.module_handler.clear_and_load(self.autonomous_key)
+        self.module_handler.launch_all()
 
 if __name__ == "__main__":
     try:
