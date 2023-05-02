61237bc716c2832d97a65d0f4f63a2e3ac543d67
Ahmed Belal <ahmedbelal.asurt@gmail.com>
2023-05-02 21:27:51 +0300

    feat: add exception classes

--- supervisor/src/supervisor/supervisor_exceptions.py
+++ supervisor/src/supervisor/supervisor_exceptions.py
@@ -63,18 +63,58 @@ class ModuleStatusError(ModuleError):
 
     """
     Subclass of ModuleError.
-    Errors related to the shutting down of a module.
+    Errors related to performing module operations in an invalid state.
     """
 
     pass
 
 # --------------------------------- Handler Sub Classes ---------------------------------
 
+
 class HandlerInitError(HandlerError):
 
     """
     Subclass of HandlerError.
     Errors related to the initializing of a ModuleHandler.
+    """
+
+    pass
+
+
+class HandlerLoadError(HandlerError):
+
+    """
+    Subclass of HandlerError.
+    Errors related to the loading of modules using a ModuleHandler.
+    """
+
+    pass
+
+
+class HandlerStatusError(HandlerError):
+
+    """
+    Subclass of HandlerError.
+    Errors related to the status of a Module from the ModuleHandler.
+    """
+
+    pass
+
+class HandlerLaunchError(HandlerError):
+
+    """
+    Subclass of HandlerError.
+    Errors related to launching Modules using a ModuleHandler.
+    """
+
+    pass
+
+
+class HandlerShutdownError(HandlerError):
+    
+    """
+    Subclass of HandlerError.
+    Errors related to shutting down Modules using a ModuleHandler.
     """
 
     pass
