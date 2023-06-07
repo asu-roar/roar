6ab6b168d1c967c9aeb252a54ffa5537fda581af
Ahmed Belal <ahmedbelal.asurt@gmail.com>
2023-06-07 14:54:25 +0300

    feat: add perception node

--- perception/perception/src/perception.py
+++ perception/perception/src/perception.py
@@ -1,8 +1,26 @@
 import rospy
+import tf
+
 
 class Handler:
     rospy.init_node("perception_node")
-    
+    rate = rospy.Rate(10)  # Publishing rate in Hz
+    # Transformation
+    x, y, z = 0, 0, 0
+    qx, qy, qz, qw = 0, 0, 0, 0
+    translation = (x, y, z)
+    rotation = (qx, qy, qz, qw)
+    br = tf.TransformBroadcaster()
+    while not rospy.is_shutdown():
+        # Publish the transform
+        br.sendTransform(
+            translation,
+            rotation,
+            rospy.Time.now(),
+            "rear_cam_frame",
+            "map")
+        rate.sleep()
+
 
 if __name__ == "__main__":
     try:
