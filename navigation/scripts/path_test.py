3b7b894585e38dfebd4a21d3c0f4627fc19b0e26
Omar Tarek Omar <tarekomar37@gmail.com>
2023-06-23 17:36:20 +0300

    re added marsyard with landmarks
    currently working on implementing the rover in the mars-yard with landmarks in order to have a descent simulation.

--- navigation/scripts/path_test.py
+++ navigation/scripts/path_test.py
@@ -5,21 +5,21 @@ from nav_msgs.msg import Path
 def path_publisher():
     rospy.init_node('path_publisher')
     pub = rospy.Publisher('/path', Path, queue_size=10)
-    rate = rospy.Rate(5)
-    rospy.sleep(1)
+    rate = rospy.Rate(10)
+    rospy.sleep()
     path = Path()
     path.header.frame_id = "map"
     pose1 = PoseStamped()
     pose1.header.stamp = rospy.Time.now()
     pose1.header.frame_id = "map"
-    pose1.pose.position.x = -2
-    pose1.pose.position.y = 4
+    pose1.pose.position.x = 10
+    pose1.pose.position.y = 12
     path.poses.append(pose1)
     pose2 = PoseStamped()
     pose2.header.stamp = rospy.Time.now()
     pose2.header.frame_id = "map"
-    pose2.pose.position.x = 2
-    pose2.pose.position.y = -4
+    pose2.pose.position.x = 6
+    pose2.pose.position.y = 3
     path.poses.append(pose2)
     path.header.stamp = rospy.Time.now()
     pub.publish(path)
@@ -35,6 +35,3 @@ if __name__ == '__main__':
 
 
 
-
-
-[2.0, 4.0]
