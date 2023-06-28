93e5c200406a09bec62e5748f18e2f8336ad06ae
Ahmed Belal <ahmedbelal.asurt@gmail.com>
2023-06-28 16:02:16 +0300

    feat: update roscan converter

--- ros_can/ros_can/src/roscan_converter.py
+++ ros_can/ros_can/src/roscan_converter.py
@@ -1,96 +1,110 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import Float32MultiArray
+from std_msgs.msg import Int8MultiArray, Float32MultiArray
 from sensor_msgs.msg import Imu
 from can_msgs.msg import Frame
 
 
 class Handler:
-    
+
     def __init__(self):
         # Initialize ROS node
         rospy.init_node("roscan_converter")
         self.rate = rospy.Rate(10)
         # Create required ROS publishers
-        self.imu_pub = rospy.Publisher("/sensors/imu", 
-                                       Imu, 
+        self.imu_pub = rospy.Publisher("/sensors/imu",
+                                       Imu,
                                        queue_size=10
                                        )
-        self.encoders_pub = rospy.Publisher("/sensors/encoders", 
-                                            Float32MultiArray, 
+        self.encoders_pub = rospy.Publisher("/sensors/encoders",
+                                            Float32MultiArray,
                                             queue_size=10
                                             )
-        self.can_pub = rospy.Publisher("/sent_messages", 
-                                       Frame, 
+        self.can_pub = rospy.Publisher("/sent_messages",
+                                       Frame,
                                        queue_size=10
                                        )
         # Initialize the received frame to None
-        self.rec_frame = None
+        self.rec_frame: Frame = None
         # Subscribe to required topics
-        rospy.Subscriber("/nav_action/supervised", 
-                         Float32MultiArray, 
+        rospy.Subscriber("/nav_action/supervised",
+                         Int8MultiArray,
                          self.nav_action_callback
                          )
-        rospy.Subscriber("/received_messages", 
-                         Frame, 
+        rospy.Subscriber("/received_messages",
+                         Frame,
                          self.can_callback
                          )
         # Run the loop
-        self.can_rerouter()
+        self.can_init()
+        self.loop()
 
     # Called every time a CAN message is received on /received_messages
-    def can_callback(self, rec_message):
+    def can_callback(self, rec_message: Frame):
         if rec_message.is_error == True:
-            rospy.logerr("Received the following CAN message with error:\n{}" 
+            rospy.logerr("Received the following CAN message with error:\n{}"
                          .format(rec_message))
         else:
-            rospy.loginfo("Received the following CAN message:\n{}" 
+            rospy.loginfo("Received the following CAN message:\n{}"
                           .format(rec_message))
             self.rec_frame = rec_message
 
     # This will be called periodically to check for received CAN frames
-    def can_rerouter(self):
+    def loop(self):
         while not rospy.is_shutdown():
             if self.rec_frame is not None:
-                # IMU message
+                # Encoders message
                 if self.rec_frame.id == 1:
-                    rospy.loginfo("IMU: Received CAN frame:\n{}" 
-                                .format(self.rec_frame)) 
-                    self.msg = Imu()
+                    rospy.loginfo("Encoders: Received CAN frame:\n{}"
+                                  .format(self.rec_frame))
+                    encoders = Int8MultiArray()
+                    encoders = self.rec_frame.data[:6]
+                    encoders = [rec_reading - 17 for rec_reading in encoders]
+                    # Insert here the packing of the received encoders CAN frame (self.rec_frame)
+                    # into std_msgs/Float32MultiArray (self.msg)
+                    self.encoders_pub.publish(encoders)
+                # IMU message
+                elif self.rec_frame.id == 2:
+                    rospy.loginfo("IMU: Received CAN frame:\n{}"
+                                  .format(self.rec_frame))
+                    msg = Imu()
                     # Insert here the packing of the received imu CAN frame (self.rec_frame)
                     # into sensor_msgs/Imu (self.msg)
-                    self.imu_pub.publish(self.msg)
-                # Encoders message
-                elif self.rec_frame.id == 2:
-                    rospy.loginfo("Encoders: Received CAN frame:\n{}" 
-                                .format(self.rec_frame))
-                    self.msg = Float32MultiArray()
-                    # Insert here the packing of the received encoders CAN frame (self.rec_frame) 
-                    # into std_msgs/Float32MultiArray (self.msg)
-                    self.encoders_pub.publish(self.msg)
+                    self.imu_pub.publish(msg)
                 # Add more if statements with new CAN ids below if needed
                 self.rec_frame = None
+                
             # else:
                 # rospy.loginfo("No CAN frames being received")
             self.rate.sleep()
-    
-    def nav_action_callback(self, rec_setpoints):
-        self.msg = Frame()
-        # Insert here the packing of the received ROS Float32MultiArray setpoints 
+
+    def nav_action_callback(self, rec_setpoints: Int8MultiArray):
+        self.can_frame.header.stamp = rospy.Time.now()
+        self.can_frame.data = rec_setpoints
+        # Insert here the packing of the received ROS Int8MultiArray setpoints
         # (rec_setpoints) into can_msgs/Frame (self.msg)
-        rospy.loginfo("Sending motor setpoints CAN frame to CAN gate:\n{}" 
-                      .format(self.msg))
-        self.can_pub.publish(self.msg)
+        rospy.loginfo("Sending motor setpoints CAN frame to CAN gate:\n{}"
+                      .format(rec_setpoints))
+        self.can_pub.publish(self.can_frame)
+
+    def can_init(self):
+        self.can_frame = Frame()
+        self.can_frame.header.frame_id = "setpoint_speeds"
+        self.can_frame.id = 0
+        self.can_frame.dlc = 6
+        self.can_frame.is_rtr = False
+        self.can_frame.is_extended = False
+        self.can_frame.is_error = False
 
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     # Run the handler by calling an object/instance
     Handler()
     # Error in case loop is terminated
     rospy.logerr("roscan_converter terminated!")
 
-    
+
 # rosrun socketcan_bridge socketcan_bridge_node _can_device:=vcan0
 
 # Receiving:
